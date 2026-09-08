622e3cc8ebc8811545baf9a5aa5328d34dfef783
Kinza Iqbal <kinzaiqbal.0101@gmail.com>
2026-09-08 07:38:17 -0700

    Delete practice.py

--- practice.py
+++
@@ -1,8 +0,0 @@
-def add(a,b):
-    return a+b
-def multiply (a,b):
-    return a*b
-x=int(input("first number:"))
-y=int(input("second number:"))
-print(f"addition:{add(x,y)}")
-print(f"multiplication:{multiply(x,y)}")
